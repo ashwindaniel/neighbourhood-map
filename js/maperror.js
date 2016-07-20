0ddd16bebc637078d7aedc53814e5fb2d1f5e1c2
Tarja M <trjmard@gmail.com>
2016-07-20 23:17:16 +0200

    Minor changes

--- js/maperror.js
+++ js/maperror.js
@@ -6,6 +6,6 @@ var mapError = function() {
 
     document.getElementById('map-error').style.display = 'block';
 
-    document.getElementById('map-error').innerHTML = 'Sorry, something is not ok. Please try again later.';
+    document.getElementById('map-error').innerHTML = 'Sorry, something went wrong. Please try again later.';
 
 };
